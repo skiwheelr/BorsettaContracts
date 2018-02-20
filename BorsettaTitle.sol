d5d7f2ca5f1b10956540b9629cca6a94188a37b9
Alpine-lines <35016946+Alpine-lines@users.noreply.github.com>
2018-02-19 23:21:54 -0700

    Add files via upload
    public/private/internal updates
    commenting fixes
    added onlyOperator modifier and operatorOf function 
    planning delegateOperator function 
    
    references;
    ethereum/EIPs#841

--- BorsettaTitle.sol
+++ BorsettaTitle.sol
@@ -1,16 +1,16 @@
 pragma solidity ^0.4.17;
                                                     //comment key
-import "./ERC721.sol";                              //#dev -- explanaition
-                                                    //#! -- issue
-contract BorsettaTitle is ERC721 {                  //#map -- key : value (pair)
-                                                    //#param -- variable            
-    //#dev limits length of titleId to 16 digits    //#move? -- place c in new contract file
-    uint256 idModulus = 10 ** 16;
-    uint256 totalTitles = borsettaTitles.length();
+import "./ERC721.sol";                                  //#notice -- explanation
+                                                        //#! -- issue
+contract BorsettaTitle is ERC721 {                      //#map -- key : value (pair)
+                                                        //#param -- variable            
+    //#notice limits length of titleId to 16 digits     //#move? -- place c in new contract file
+    uint256 internal idModulus = 10 ** 16;              //#dev issue/task explanation/question
+    uint256 private totalTitles = borsettaTitles.length();
 
     struct DiamondTitle {
-        //#!variables will be revised shortly
-        uint256 id; //#paramhash id generated from xyz
+        //#! variables will be revised shortly
+        uint256 id; //#param hash id generated from xyz
         uint256 date; //#param x
         uint8 weight; //#param y
         uint8 quality; //#param z
@@ -19,35 +19,41 @@ contract BorsettaTitle is ERC721 {                  //#map -- key : value (pair)
     }
     
     DiamondTitle[] public borsettaTitles;
-    
-    //#devtitleId is a sha3 hash generated from xyz
-    //#devhere titleID is mapped to the index of that title in borsettaTitles array
+    //#notice here titleID is mapped to the index of that title in borsettaTitles array    
+    //#param titleId is a sha3 hash generated from xyz
     //#map titleId : bosettaTitles[_index];
-    mapping (uint256 => uint) titleIdIndex;
+    mapping (uint256 => uint) private titleIdIndex;
     //#map titleId : address
-    mapping (uint256 => address) titleToOwner;
+    mapping (uint256 => address) internal titleToOwner;
     //#map adress : list of owned tokes
     mapping (address => uint256[]) private ownedTitles;
     //#map titleId : owneTitles[index]
     mapping (uint256 => uint256) private ownedTitlesIndex;    
-    // Mapping from token ID to approved address
+    //#map titleID : approved address
     mapping (uint256 => address) private titleApprovals;
+    //#map titleId : approvedOperator
+    mapping (uint256 => address) private titleOperator;
 
     event TitleMinted(uint id, uint date, uint weight, uint quality, string labName);
     //#TBD event TitleDataVerified(); -- ?one event for all _verify functions?  
-            //?or unique event for each _verify function?
+            //                      or unique event for each _verify function?
     //#TBD event OperatorAuthorized(address indexed operator, address indexed owner);
-    event TitleTransfer(address indexed from, address indexed to, uint256 tokens);
-    event Approval(address indexed from, address indexed to, uint256 indexed deedId);
+    event TitleTransfer(address indexed from, address indexed to, uint256 titles);
+    event Approval(address indexed from, address indexed to, uint256 indexed id);
     event Delegation(address indexed _owner, address indexed _operator); 
 
     modifier onlyOwnerOf(uint256 _id) {
         require(ownerOf(_id) == msg.sender);
         _;
     }
-    //#dev mints new title with originator specified data
-    //#dev set id to uint256 version of result of sha3(_date, _weight, _quality)
-    //#dev _date is set to current block height
+
+     modifier onlyOperator(uint256 _id) {
+        require(operatorOf(_id) == msg.sender);
+        _;
+    }
+    //#notice mints new title with originator specified data
+    //#param sets id to uint256 version of result of sha3(_date, _weight, _quality)
+    //#param _date is set to current block height
     function _mintTitle(uint8 _weight, uint8 _quality, string _labName) private {
         uint256 _date = block.height;
         uint256 id = uint256(sha3(_date, _weight, _quality)) % idModulus;
@@ -59,8 +65,8 @@ contract BorsettaTitle is ERC721 {                  //#map -- key : value (pair)
         TitleMinted(id, _date, _weight, _quality, _labName);
     }
 
-    //#dev assigns an authorized operator
-    function delegate(address _delegate) external {
+    //#notice assigns an authorized operator
+    function delegateOperator(address _operator) external {
 
     }    
     //#move? cross references orignator input against test lab data, quantifies and lists discrepencies
@@ -91,41 +97,47 @@ contract BorsettaTitle is ERC721 {                  //#map -- key : value (pair)
     }
     }
     
-    #dev function to allow external operators contracts to track possesion data
+    #notice function to allow external operators contracts to track possesion data
 
     function _trackPossesion(string _operatorName) {
 
     }
     */
 
-  //#dev returns number of titles associated with address   
+  //#notice returns number of titles associated with address   
   function balanceOf(address _owner) public view returns (uint256) {
       return ownedTitles[_owner].length;
   }
   
-  //#dev returns list of titles by titleId (hash id) owned by given address
+  //#notice returns list of titles by titleId (hash id) owned by given address
   function titlesOf(address _owner) public view returns (uint256[]) {
     return ownedTitles[_owner];
   }
-  //#dev returns owner of titleId
+  //#notice returns owner of titleId
   function ownerOf(uint256 _id) public view returns (address) {
     address owner = titleToOwner[_id];
     require(owner != address(0));
     return owner;
   }
-  //#dev returns number of titles total
+  //#notice returns operator of titleId
+  function operatorOf(uint256 _id) public view returns (address) {
+      address operator = titleOperator[_id];
+      require(operator != address(0));
+      return operator;
+  }
+  //#notice returns number of titles total
   function countOfTitles() external view returns (uint256 _count) {
       return borsettaTitles.length();
   }
-  //#dev returns number of titles held by owner
+  //#notice returns number of titles held by owner
   function countOfTitlesByOwner(address _owner) external view returns (uint256 _count) {
       require(_owner != address(0));
       return ownedTitles[_owner].length();
   }
-  //#dev returns titleId of a given adress by index (0 = 1st item in owndedTitles[address])
-  //#dev does not use titleId because owndTitles is mapping of an address to an array
-  //#dev not an address to mapping. Using a nested mapping would allow us to replace the index
-  //#dev with a key which we would set to the titleID.
+  //#notice returns titleId of a given adress by index (0 = 1st item in owndedTitles[address])
+  //#dev does not use titleId because ownedTitles is mapping of an address to an array
+  //#dev not a mapping of address to mapping. Using a nested mapping would allow us to replace the 
+  //#dev index with a key which we would set to the titleID.
   //#dev However, I do not know if using a nested mapping here would be best or not.
   function titleOfOwnerByIndex(address _owner, uint256 _index) external view returns (uint256 _deedId) {
       require(_index <= countOfTitlesByOwner(_owner));
@@ -208,7 +220,6 @@ contract BorsettaTitle is ERC721 {                  //#map -- key : value (pair)
     ownedTitlesIndex[lastToken] = index;
     totalTitles = totalTitles.sub(1);
   }
-  
   //#TBD function supportsInterface(bytes4 interfaceID) external view returns (bool) {
 
   //}
